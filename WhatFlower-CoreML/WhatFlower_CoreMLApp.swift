b42fe7b77f8c4581f2d0e1648f33eb12032dbeba
Alexander Senyuk <a.senyuk314@gmail.com>
2022-01-13 12:46:16 +0300

    Initial Commit

---
+++ WhatFlower-CoreML/WhatFlower_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatFlower_CoreMLApp.swift
+//  WhatFlower-CoreML
+//
+//  Created by Александр Сенюк on 13.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatFlower_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
